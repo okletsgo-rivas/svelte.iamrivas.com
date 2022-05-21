4e2704cba8dc2b8de1da098f97903321901632ba
John Rivas <jrivas@cranemorley.com>
2022-05-20 19:47:33 -0700

    Homepage redirect fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
-  import { Router, Route, navigate } from "svelte-routing";
+  import { onMount } from "svelte";
+
+  import { Router, Route, navigate, link } from "svelte-routing";
   import Header from "./components/layout/Header.svelte";
   import PageBio from "./components/pages/PageBio.svelte";
   import PageProjects from "./components/pages/PageProjects.svelte";
   import PageResume from "./components/pages/PageResume.svelte";
 
   export let url = "";
+
+  onMount(() => {
+    if (window.location.pathname == "/") navigate("/projects");
+  });
 </script>
 
 <main>
   <Router {url}>
     <div class="app">
       <Header />
-      <Route path="/">{navigate("/projects")}</Route>
+      <Route path="/">Redirecting</Route>
       <Route path="projects" component={PageProjects} />
       <Route path="bio" component={PageBio} />
       <Route path="resume" component={PageResume} />
